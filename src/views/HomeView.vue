d6f79bf8d6916bce9f6ee872743e591b73998e81
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-02-25 20:45:13 -0600

    chore(whole app): :package: added drag n drop library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const [parent, tapes] = useDragAndDrop([
+  'Depeche Mode',
+  'Duran Duran',
+  'Pet Shop Boys',
+  'Kraftwerk',
+  'Tears for Fears',
+  'Spandau Ballet'
+])
+</script>
 
 <template>
-  <main>
-    <h1>Hello Clique !</h1>
+  <main class="h-screen flex mx-auto w-full flex-col items-center">
+    <h1 class="font-mono text-4xl mt-10 text-center">twenty øne piløts ranking songs</h1>
   </main>
 </template>
